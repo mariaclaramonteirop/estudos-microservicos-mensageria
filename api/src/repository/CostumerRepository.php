7af74c7df1bbe0e66c756feabc931c3916f8af8e
Maria Clara Monteiro <mcmpacheco7@gmail.com>
2026-08-25 13:09:32 -0300

    feat: adiciona interfaces CostumerRepository, OrderRepository e ProductRepository com métodos de gerenciamento

---
+++ api/src/repository/CostumerRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Costumer;
+
+interface CostumerRepository
+{
+    public function findById(int $id): ?Costumer;
+    public function findAll(): array;
+    public function save(Costumer $costumer): void;
+    public function delete(int $id): void;
+    public function update(Costumer $costumer): void;
+    public function findByRestrictions(array $restrictions): array;
+}
---
+++ api/src/repository/ProductRepository..php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Order;
+
+interface OrderRepository
+{
+    public function findById(int $id): ?Order;
+    public function findAll(): array;
+    public function save(Order $order): void;
+    public function delete(int $id): void;
+    public function update(Order $order): void;
+    public function findByRestrictions(array $restrictions): array;
+}
